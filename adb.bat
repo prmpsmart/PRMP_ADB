cdb3f9f6f64648d2a2fcdf611f037bbe3b72a667
prmpsmart <prmpsmart@gmail.com>
2021-06-13 00:24:29 +0100

    removed __pycache__

--- adb.bat
+++ adb.bat
@@ -7,6 +7,11 @@ adb32\adb root
 @REM adb32\adb push "C:\Users\Administrator\Videos\apata\the big bang theory\season 8" /sdcard/Miracle/Big_Bang_Theory
 @REM adb32\adb push "C:\Users\Administrator\Videos\apata\the big bang theory\season 9" /sdcard/Miracle/Big_Bang_Theory
 @REM adb32\adb push "C:\Users\Administrator\Videos\apata\the big bang theory\season 10" /sdcard/Miracle/Big_Bang_Theory
-adb32\adb shell ls /sdcard -pRhs
+@REM adb32\adb shell ls /sdcard -pRhs
 @REM adb32\adb devices -l
+
+
+@REM git remote add origin https://github.com/prmpsmart/PRMP_ADB.git
+@REM git branch -M main
+@REM git push -u origin main
 
